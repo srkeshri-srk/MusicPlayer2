87289adaec756c0be6f26df962e1e08f639d827a
Shreyansh <Srkeshri.srk@gmail.com>
2023-10-17 15:11:53 +0530

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Shreyansh Raj  Keshri on 17/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
